d1e21a16b1df9e0ee7986fc2a5a746beaefc07ce
RegmiS <sandeshreg35@gmail.com>
2024-10-21 16:36:17 -0500

    Create sample folder for outlining webpage

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
 import "./styles.css";
+
+const createNav = () => {
+    const navEl = document.createElement("nav");
+
+    const buttonHome = document.createElement("button");
+    buttonHome.innerHTML = "Home"
+    navEl.appendChild(buttonHome);
+
+    const buttonContent = document.createElement("button");
+    buttonContent.innerHTML = "Content"
+    navEl.appendChild(buttonContent);
+
+    const buttonContact = document.createElement("button");
+    buttonContact.innerHTML = "Contact"
+    navEl.appendChild(buttonContact);
+    return navEl;
+};
+
+const contentEl = document.querySelector("#content");
+contentEl.appendChild(createNav());
